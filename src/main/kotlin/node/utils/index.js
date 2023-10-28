b33858aa9cfcaaef4d50f67867e15ff16aaf8eb2
huvz <dachen04@outlook.com>
2023-10-28 21:44:42 +0800

    :star:update基础数据

---
+++ src/main/kotlin/node/utils/index.js
@@ -0,0 +1,15 @@
+// @ts-check
+const { RSAUtils } = require("./rsa");
+module.exports = {
+  getNum(str) {
+    var num = str.match(/\d+/);
+    return num?.length > 0 ? num[0] : "";
+  },
+
+  encryptPwd(password, exponent, modulus) {
+    const key = new RSAUtils.getKeyPair(exponent, "", modulus);
+    const reversedPwd = password.split("").reverse().join("");
+    const encrypedPwd = RSAUtils.encryptedString(key, reversedPwd);
+    return encrypedPwd;
+  },
+};
