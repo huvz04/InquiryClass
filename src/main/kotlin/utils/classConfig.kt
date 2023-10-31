61724e23b45f8d83085d2c1f2b59ea9abe64ee25
huvz <dachen04@outlook.com>
2023-11-01 01:48:13 +0800

    :blue_book:教务网信息提取；完成80%

--- src/main/kotlin/utils/classConfig.kt
+++ src/main/kotlin/utils/classConfig.kt
@@ -7,7 +7,7 @@ object classConfig
    //val username :Long  by value(2022123456789);
     val username :String  = "202120201550111";
     //val password:String by value("12345678")
-    val password:String = "A@ww20030401"
+
 
 
 }
